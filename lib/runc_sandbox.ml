eab6311a69810e1a67c89ca63b8a4c9d924ff2bf
Thomas Leonard <talex5@gmail.com>
2021-04-06 15:52:57 +0100

    Limit permissions on temporary directories

--- lib/runc_sandbox.ml
+++ lib/runc_sandbox.ml
@@ -280,7 +280,7 @@ let copy_to_log ~src ~dst =
   aux ()
 
 let run ~cancelled ?stdin:stdin ~log t config results_dir =
-  Lwt_io.with_temp_dir ~prefix:"obuilder-runc-" @@ fun tmp ->
+  Lwt_io.with_temp_dir ~perm:0o700 ~prefix:"obuilder-runc-" @@ fun tmp ->
   let json_config = Json_config.make config ~config_dir:tmp ~results_dir t in
   Os.write_file ~path:(tmp / "config.json") (Yojson.Safe.pretty_to_string json_config ^ "\n") >>= fun () ->
   Os.write_file ~path:(tmp / "hosts") "127.0.0.1 localhost builder" >>= fun () ->
