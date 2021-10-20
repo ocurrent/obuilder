d46d62cad2d2d33d47999d34abaa3f53a2c0062d
Antonin Décimo <antonin@tarides.com>
2021-10-20 16:14:48 +0200

    Replace Travis with GitHub Actions

--- .run-gha-tests.sh
+++ .run-gha-tests.sh
@@ -2,7 +2,7 @@
 set -eux
 export OPAMYES=true
 
-sudo wget https://github.com/opencontainers/runc/releases/download/v1.0.0-rc92/runc.amd64 -O /usr/local/bin/runc
+sudo wget https://github.com/opencontainers/runc/releases/download/v1.0.2/runc.amd64 -O /usr/local/bin/runc
 sudo chmod a+x /usr/local/bin/runc
 
 sudo sh -c "cat > /usr/local/bin/uname" << EOF
@@ -18,26 +18,24 @@ sudo chmod a+x /usr/local/bin/uname
 
 dd if=/dev/zero of=/tmp/zfs.img bs=100M count=50
 ZFS_LOOP=$(sudo losetup -f)
-sudo losetup -P $ZFS_LOOP /tmp/zfs.img
-sudo zpool create zfs $ZFS_LOOP
+sudo losetup -P "$ZFS_LOOP" /tmp/zfs.img
+sudo zpool create zfs "$ZFS_LOOP"
 
 dd if=/dev/zero of=/tmp/btrfs.img bs=100M count=50
 BTRFS_LOOP=$(sudo losetup -f)
-sudo losetup -P $BTRFS_LOOP /tmp/btrfs.img
-sudo mkfs.btrfs -f $BTRFS_LOOP
+sudo losetup -P "$BTRFS_LOOP" /tmp/btrfs.img
+sudo mkfs.btrfs -f "$BTRFS_LOOP"
 sudo mkdir /btrfs
-sudo mount -t btrfs $BTRFS_LOOP /btrfs
-sudo chown $(whoami) /btrfs
+sudo mount -t btrfs "$BTRFS_LOOP" /btrfs
+sudo chown "$(whoami)" /btrfs
 
-[ -d ~/.opam/4.11.1 ] || opam init --compiler=4.11.1
-opam install --deps-only -t .
 opam exec -- make
 opam exec -- dune exec -- obuilder healthcheck --store=btrfs:/btrfs
 opam exec -- dune exec -- obuilder healthcheck --store=zfs:zfs
 opam exec -- dune exec -- ./stress/stress.exe btrfs:/btrfs
 opam exec -- dune exec -- ./stress/stress.exe zfs:zfs
 
-# Populate the caches from our own Travis cache
+# Populate the caches from our own GitHub Actions cache
 btrfs subvolume create /btrfs/cache/c-opam-archives
 cp -r ~/.opam/download-cache/* /btrfs/cache/c-opam-archives/
 sudo chown -R 1000:1000 /btrfs/cache/c-opam-archives
--- example.spec
+++ example.spec
@@ -12,7 +12,10 @@
 	 (user (uid 1000) (gid 1000))                           ; Build as the "opam" user
 	 (run (shell "sudo chown opam /src"))
 	 (env OPAM_HASH "3332c004db65ef784f67efdadc50982f000b718f") ; Fix the version of opam-repository we want
-	 (run 
+	 (run
+	  (network host)
+	  (shell "sudo apt-get --allow-releaseinfo-change update"))
+	 (run
 	  (network host)
 	  (shell
 	   "cd ~/opam-repository \
