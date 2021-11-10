8b4828ff4097191fd235736a2dd4494130a58e99
Antonin Décimo <antonin@tarides.com>
2021-11-10 16:08:19 +0100

    Build_log simplifications
    - don't duplicate the log file file descriptor but switch to reading
      the file when the log finishes;
    - replace the `Finished state with the `Readonly state.

--- lib/build_log.ml
+++ lib/build_log.ml
@@ -4,20 +4,12 @@ let max_chunk_size = 4096
 
 type t = {
   mutable state : [
-    | `Open of Lwt_unix.file_descr * unit Lwt_condition.t  (* Fires after writing more data. *)
+    | `Open of string * Lwt_unix.file_descr * unit Lwt_condition.t  (* Fires after writing more data. *)
     | `Readonly of string
     | `Empty
-    | `Finished
   ];
   mutable len : int;
 }
-
-let with_dup fd fn =
-  let fd = Lwt_unix.dup fd in
-  Lwt_unix.set_close_on_exec fd;
-  Lwt.finalize
-    (fun () -> fn fd)
-    (fun () -> Lwt_unix.close fd)
 
 let catch_cancel fn =
   Lwt.catch fn
@@ -27,24 +19,25 @@ let catch_cancel fn =
     )
 
 let tail ?switch t dst =
-  match t.state with
-  | `Finished -> invalid_arg "tail: log is finished!"
-  | `Readonly path ->
+  let readonly_tail path buf i =
     Lwt_io.(with_file ~mode:input) path @@ fun ch ->
-    let buf = Bytes.create max_chunk_size in
+    Lwt_io.set_position ch (Int64.of_int i) >>= fun () ->
     let rec aux () =
       Lwt_io.read_into ch buf 0 max_chunk_size >>= function
       | 0 -> Lwt_result.return ()
       | n -> dst (Bytes.sub_string buf 0 n); aux ()
     in
+    aux ()
+  in
+  match t.state with
+  | `Readonly path ->
+    let buf = Bytes.create max_chunk_size in
     catch_cancel @@ fun () ->
-    let th = aux () in
+    let th = readonly_tail path buf 0 in
     Lwt_switch.add_hook_or_exec switch (fun () -> Lwt.cancel th; Lwt.return_unit) >>= fun () ->
     th
   | `Empty -> Lwt_result.return ()
-  | `Open (fd, cond) ->
-    (* Dup [fd], which can still work after [fd] is closed. *)
-    with_dup fd @@ fun fd ->
+  | `Open (_, fd, cond) ->
     let buf = Bytes.create max_chunk_size in
     let rec aux i =
       match switch with
@@ -52,9 +45,13 @@ let tail ?switch t dst =
       | _ ->
         let avail = min (t.len - i) max_chunk_size in
         if avail > 0 then (
-          Lwt_unix.pread fd ~file_offset:i buf 0 avail >>= fun n ->
-          dst (Bytes.sub_string buf 0 n);
-          aux (i + avail)
+          match t.state with
+          | `Open _ ->
+            Lwt_unix.pread fd ~file_offset:i buf 0 avail >>= fun n ->
+            dst (Bytes.sub_string buf 0 n);
+            aux (i + avail)
+          | `Readonly path -> readonly_tail path buf i
+          | _ -> Lwt_result.return ()
         ) else (
           match t.state with
           | `Open _ -> Lwt_condition.wait cond >>= fun () -> aux i
@@ -70,28 +67,25 @@ let create path =
   Lwt_unix.openfile path Lwt_unix.[O_CREAT; O_TRUNC; O_RDWR; O_CLOEXEC] 0o666 >|= fun fd ->
   let cond = Lwt_condition.create () in
   {
-    state = `Open (fd, cond);
+    state = `Open (path, fd, cond);
     len = 0;
   }
 
 let finish t =
   match t.state with
-  | `Finished -> invalid_arg "Log is already finished!"
-  | `Open (fd, cond) ->
-    t.state <- `Finished;
+  | `Open (path, fd, cond) ->
+    t.state <- `Readonly path;
     Lwt_unix.close fd >|= fun () ->
     Lwt_condition.broadcast cond ()
   | `Readonly _ ->
-    t.state <- `Finished;
     Lwt.return_unit
   | `Empty ->
     Lwt.return_unit (* Empty can be reused *)
 
 let write t data =
   match t.state with
-  | `Finished -> invalid_arg "write: log is finished!"
   | `Readonly _ | `Empty -> invalid_arg "Log is read-only!"
-  | `Open (fd, cond) ->
+  | `Open (_, fd, cond) ->
     let len = String.length data in
     Os.write_all fd (Bytes.of_string data) 0 len >>= fun () ->
     t.len <- t.len + len;
