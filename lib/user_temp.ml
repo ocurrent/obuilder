8283b9c927e6bfc7eac3916f148fddf18a6495a4
Patrick Ferris <pf341@patricoferris.com>
2021-11-15 10:31:27 +0000

    make Os.Macos a top-level module

--- lib/user_temp.ml
+++ lib/user_temp.ml
@@ -2,7 +2,7 @@ open Lwt.Infix
 
 let fetch ~log:_ ~rootfs base =
   let base = Filename.concat "/Users" base in
-  Os.Macos.copy_template ~base ~local:rootfs >>= fun _ ->
+  Macos.copy_template ~base ~local:rootfs >>= fun _ ->
   Os.sudo [ "chown"; "-R"; ":1000"; rootfs ] >>= fun () ->
   Os.sudo [ "chmod"; "-R"; "g+w"; rootfs ] >>= fun () ->
   Lwt.return []
