20229cbdae4dc55b3f1ca5e68c6c48e337dd91d7
Tim McGilchrist <timmcgil@gmail.com>
2022-10-10 15:48:24 +1100

    Create tmp dir and fixup macos depends in dune.

--- test/macos/macos.sh
+++ test/macos/macos.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mkdir -p /tmp/scoreboard
-../../main.exe build . -f ./specs/simple.macos.spec --uid=705 --store=rsync:/tmp/rsync --fallback=/ --scoreboard=/tmp/scoreboard --no-fuse
-../../main.exe build . -f ./specs/simple.macos.spec --uid=705 --store=rsync:/tmp/rsync --fallback=/ --scoreboard=/tmp/scoreboard --no-fuse
-sudo rm -rf /tmp/rsync
+TMP_SCOREBOARD=$(mktemp -d 2>/dev/null || mktemp -d -t 'scoreboard')
+TMP_RSYNC=$(mktemp -d 2>/dev/null || mktemp -d -t 'rsync')
+dune exec -- obuilder build . -f ./specs/simple.macos.spec --uid=705 --store=rsync:$TMP_RSYNC --fallback=/ --scoreboard=$TMP_SCOREBOARD --no-fuse
+dune exec -- obuilder build . -f ./specs/simple.macos.spec --uid=705 --store=rsync:$TMP_RSYNC --fallback=/ --scoreboard=$TMP_SCOREBOARD --no-fuse
+rm -rf $TMP_SCOREBOARD $TMP_RSYNC
